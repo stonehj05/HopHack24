a30856b387ffc001202e830eeda4a916081c20df
江华 <harry@harrys-macbook.acasmart.jh.edu>
2024-09-14 22:48:02 -0400

    step finish of main.py

--- picture.py
+++ picture.py
@@ -6,7 +6,7 @@ def image_display(fileName, n):
     for i, col in enumerate(cols):
         with col:
             st.image(fileName, width=100)
-            if st.button(st.session_state.courseDictionary[i+1]):
+            if st.button(st.session_state.courseDictionary[i+1], key=f"button_{i}" + str(st.session_state.courseIndex)):
                 st.session_state.currentCourse  = st.session_state.courseDictionary[i+1]
                 st.switch_page("pages/course_page.py")
 
